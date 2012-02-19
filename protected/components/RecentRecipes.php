8dcb04c72f0b29c389afcd6c21e6972a99df67d4
Carl-Anton Ingmarsson <ca.ingmarsson@gmail.com>
2012-02-19 22:26:46 +0100

    Properly register portlets.css

--- protected/components/RecentRecipes.php
+++ protected/components/RecentRecipes.php
@@ -2,34 +2,36 @@
 
 Yii::import('zii.widgets.CPortlet');
 Yii::import('zii.widgets.grid.CGridView');
- 
+
 class RecentRecipes extends CPortlet
 {
-    public $title='Senaste recept';
-    public $maxRecipes=5;
-    public $contentCssClass='recent-recipes portlet-content';
- 
-    public function init()
-    {
-      $uri = '/test/css/portlets.css';
-      Yii::app()->clientScript->registerCssFile($uri, 'screen, projection');
-      return parent::init();
-    }
-    protected function renderContent()
-    {
-        $criteria = array(
-            'order'=>'id DESC',
-            'select'=>'id,title',
-            'limit'=>$this->maxRecipes,
-        );
 
-        $recipes = Recipe::model()->findAll($criteria);
-        
-        echo '<ul>';
-        foreach ($recipes as $recipe)
-        {
-            echo '<li>' . CHtml::link($recipe->title, array('recipe/view', 'recipeID'=>$recipe->id)) . '</li>';
-        }
-        echo '</ul>';
+  public $title = 'Senaste recept';
+  public $maxRecipes = 5;
+  public $contentCssClass = 'recent-recipes portlet-content';
+
+  public function init()
+  {
+    $url = Yii::app()->createUrl('css/portlets.css');
+    Yii::app()->clientScript->registerCssFile($url, null);
+    return parent::init();
+  }
+
+  protected function renderContent()
+  {
+    $criteria = array(
+        'order' => 'id DESC',
+        'select' => 'id,title',
+        'limit' => $this->maxRecipes,
+    );
+
+    $recipes = Recipe::model()->findAll($criteria);
+
+    echo '<ul>';
+    foreach ($recipes as $recipe) {
+      echo '<li>' . CHtml::link($recipe->title, array('recipe/view', 'recipeID' => $recipe->id)) . '</li>';
     }
+    echo '</ul>';
+  }
+
 }
